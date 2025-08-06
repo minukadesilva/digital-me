8bbbd4e6a105374c6960c5a9d0a4afea29e5954f
Minuka De Silva <100896047+minukadesilva@users.noreply.github.com>
2025-08-06 15:54:53 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 //Hamburger section
 
 const hamburger = document.getElementById("hamburger");
+const closeIcon = document.getElementById("close-icon");
 const navLinks = document.getElementById("nav-links");
 const socialIcons = document.getElementById("social-icons");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   socialIcons.classList.toggle("show");
+  hamburger.classList.add("hide");
+  closeIcon.classList.add("show");
+});
+
+closeIcon.addEventListener("click", () => {
+  navLinks.classList.remove("show");
+  socialIcons.classList.remove("show");
+  closeIcon.classList.remove("show");
+  hamburger.classList.remove("hide");
 });
 
 //Contact alert section
